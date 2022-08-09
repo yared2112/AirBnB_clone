2f2ea891ffea5ad95d56a73aa47b70e22379adc0
YaredT21 <teklewold.yared21@gmail.com>
2022-08-10 02:53:01 +0300

    completed task

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# AirBnB_clone
+AirBnB Clone
+A simple command line interpreter to be used to add, modify, or delete information in the AirBnB Clone database.
+
+Project Notes
+
+Directories and Files
+Directories
+models: holds the modules and methods to be used with console.py
+tests: holds the unittests for all modules
+Files: Programs
+HBNHCommand: console.py
+Amenity: models/amenity.py
+BaseModel: models/base_model.py
+City: models/city.py
+models.init : models/__init__.py
+Place: models/place.py
+Review: models/review.py
+State: models/state.py
+User: models/user.py
+FileStorage: models/engine/file_storage.py
+engine.init: models/engine/__init__.py
+
+
+Files: Test Files
+test_console.py: tests/
+test_amenity.py: tests/test_models/
+test_base_model.py: tests/test_models/
+test_city.py: tests/test_models/
+test____init__.py: tests/test_models/
+test_place.py: tests/test_models/
+test_review.py: tests/test_models/
+test_state.py: tests/test_models/
+test_user.py: tests/test_models/
+test_file_storage.py: tests/test_models/test_engine
---
+++ models/engine/file_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import uuid
+import json
+
+'File storage module'
+
+
+class FileStorage:
+    'FileStorage class'
+    __file_path = "./file.json"
+    __objects = {}
+
+    def all(self):
+        'Returns __objects'
+        return self.reload()
+
+    def new(self, obj):
+        'adds an object to the instance'
+        from models.base_model import BaseModel
+        import uuid
+
+        class_name = str(obj.__class__.__name__)
+        obj_id = str(obj.id)
+        obj_str = class_name + "." + obj_id
+        FileStorage.__objects[obj_str] = obj
+
+    def save(self):
+        'serializes __objects to JSON file'
+        from models.base_model import BaseModel
+        new_dict = {}
+
+        for keys in FileStorage.__objects.keys():
+            new_dict[keys] = (FileStorage.__objects[keys]).to_json()
+
+        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        'deserializes the JSON file to __objects'
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.city import City
+        from models.place import Place
+        from models.state import State
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                   'State': State, 'City': City, 'Amenity': Amenity,
+                   'Review': Review}
+        try:
+            with open(FileStorage.__file_path,
+                      mode="r", encoding="utf-8") as f:
+                reloaded = json.load(f)
+                for k, v in reloaded.items():
+                    class_n = classes.get(reloaded[k].get('__class__'))
+                    self.__objects[k] = class_n(**reloaded[k])
+                return self.__objects
+        except:
+            return {}
